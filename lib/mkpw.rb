786fbb44d7f382e6390eba9a6cdde20ecf0e615e
Eric Jacobs <ichthyosaur@slush.systems>
2017-07-28 19:20:33 -0600

    a

--- lib/mkpw.rb
+++ lib/mkpw.rb
@@ -1,24 +1,22 @@
 #!/usr/bin/env ruby
 
 class Mkpw
-  attr_accessor :components
-  def initialize
-    @components = {
-      words: File.readlines('/usr/share/dict/words'),
-      nums: (0..9).to_a,
-      alphas: ('a'..'z').to_a + ('A'..'Z').to_a,
-      symbols: ',./<>?;:-=_+&*%^#$!@~|'.chars
-    }
-    true
+  attr_reader :words, :nums, :alphas, :symbols, :components
+  def initialize(alphas: nil, nums: nil, symbols: nil, words: nil)
+    @words = (words || File.readlines('/usr/share/dict/words')).freeze
+    @nums = (nums || (0..9).to_a).freeze
+    @alphas = (alphas || ('a'..'z').to_a + ('A'..'Z').to_a).freeze
+    @symbols = (symbols || ',./<>?;:-=_+&*%^#$!@~|'.chars).freeze
+    @components = { alphas: @alphas, nums: @nums, words: @words, symbols: @symbols }.freeze
   end
 
   def generate_word_pass(word_qty: 2)
-    {seperator: compose.join, words: components[:words].sample(word_qty).map(&:chomp)}
+    {seperator: compose.join, words: words.sample(word_qty).map(&:chomp)}
   end
 
   def compose(width: 2, weights: {nums: 0.5, symbols: 0.5})
     total = 0
     weights.map {|set,weight| total += weight}
-    weights.map {|set,weight| components[set].sample((weight / total) * width)}.shuffle
+    weights.map {|set,weight| components[set.to_sym].sample((weight / total) * width)}.shuffle
   end
 end
