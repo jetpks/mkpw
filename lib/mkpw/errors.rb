8f92e8fbb972780ff8d47080bd78a588c7dcabb2
Eric Jacobs <ejacobs@instructure.com>
2019-07-10 11:52:16 -0600

    fixup! Add Constituents class and associated tests

--- lib/mkpw/errors.rb
+++ lib/mkpw/errors.rb
@@ -6,4 +6,10 @@ module Mkpw
       super "Unable to read file at '#{path}'; does it exist?"
     end
   end
+
+  class NotAString < MkpwError
+    def initialize(item)
+      super "All constituent items must be Strings. Received item '#{item}', which is a #{item.class}"
+    end
+  end
 end
