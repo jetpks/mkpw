899fba5722260a051211eaed95d3921a9f259774
Eric Jacobs <ejacobs@instructure.com>
2019-07-10 12:40:24 -0600

    Add numbers class and tests

---
+++ lib/mkpw/numbers.rb
@@ -0,0 +1,9 @@
+module Mkpw
+  DEFAULT_NUMBERS = (0..9).map(&:to_s).freeze
+
+  class Numbers < Constituents
+    def initialize(add: nil, only: nil)
+      super(DEFAULT_NUMBERS, add: add, only: only)
+    end
+  end
+end
